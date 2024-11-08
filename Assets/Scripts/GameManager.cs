76c3b8e84e355a1037e13f6ebed75cb58e9139c5
Jfritz03 <jfritz3@student.fitchburgstate.edu>
2024-11-08 16:17:40 -0500

    Coding
    Fixed Code errors in the Game Manager script.
    
    Revised pusher script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float P1Lives = 3;
+    public float P2Lives = 3;
+    public GameObject Player1Prefab;
+    public GameObject Player2Prefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,30 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Escape))
         {
             Application.Quit();
+        }
+        // resets player position when falling off map while removing a life
+        if (Player1Prefab.transform.position.y < -5 && P1Lives > 1)
+        {
+            Player1Prefab.transform.position = new Vector3(-31, 11, -79);
+            P1Lives--;
+        }
+        if (Player1Prefab.transform.position.y < -5 && P1Lives == 1)
+        {
+            Destroy(Player1Prefab);
+            P1Lives--;
+            //Display player 2 win screen
+        }
+        // player 2 section
+        if (Player2Prefab.transform.position.y < -5 && P2Lives > 1)
+        {
+            Player2Prefab.transform.position = new Vector3(144, 11, 87);
+            P2Lives--;
+        }
+        if (Player2Prefab.transform.position.y < -5 && P2Lives == 1)
+        {
+            Destroy(Player2Prefab);
+            P2Lives--;
+            //Display player 1 win screen
         }
     }
 }
---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    private float pushStrength = 30.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody PlayerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            PlayerRigidbody.AddForce(awayFromPlayer * pushStrength, ForceMode.Impulse);
+            Debug.Log("Player Collision");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
